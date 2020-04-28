eebba86168e5b77ee4bfbdc601c04210f986dcf1
minwoo <minwoo@daegu.ac.kr>
2020-04-28 18:05:25 +0900

    conflict resolved

--- src/com/pad/noob/AboutWindow.java
+++ src/com/pad/noob/AboutWindow.java
@@ -12,8 +12,9 @@ public class AboutWindow {
 		frame.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 30));
 		frame.setIconImage(noobPad.getLogo().getImage());
 		frame.add(new JLabel(noobPad.getLogoX2()));
-		frame.add(new JLabel("NoobPad v1"));
-		frame.setTitle("About"); 
+		
+		frame.add(new JLabel("<html><body>제작자 : babibux<br>깃허브 : <a href=\" https://github.com/babibux/NoobPad\">https://github.com/babibux/NoobPad</a><br>수정자 : 최민우<br>수정내용 : 아이콘변경, 한글화, 정보란 수정, 서식툴(배경색상, 글자색상 변경) </body></html>"));
+		frame.setTitle("정보"); 
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.pack();
